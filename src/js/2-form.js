1b09eb92cbc105d1cd208f84c46a1ea0351b637f
Olena Oleksandrova <ha240886kev@gmail.com>
2024-09-10 22:34:32 +0300

    js_form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -6,23 +6,67 @@
 // function setStorageData(event) {
 //     localStorage.setItem('elem', event.target.className);
 // }
+document.addEventListener('DOMContentLoaded', () => {
 
-const STORAGE_KEY = 'feedback-msg';
+    const STORAGE_KEY = 'feedback-form-state';
 
-const form = document.querySelector('.feedback-form');
-const textarea = form.querySelector('.textarea');
+    let formData = { email: '', message: '' };
 
-form / addEventListener('submit', handleFormSubmit);
+    const savedData = localStorage.getItem(STORAGE_KEY);
+    if (savedData) {
+        try { 
+        formData = JSON.parse(savedData);
+        form.email.value = formData.email || '';
+        form.message.value = formData.message || '';
+        } catch (error) {  
+    }
+ }
+    const form = document.querySelector('.feedback-form');
+    const textarea = form.querySelector('textarea');
 
-function handleFormSubmit(event) {
-    event.preventDefault();
-    const form = event.currentTarget;
-    localStorage.removeItem(STORAGE_KEY);
+    form.addEventListener('submit', handleFormSubmit);
+
+
+    function handleFormSubmit(event) {
+        event.preventDefault();
+
+        // formData[event.target.name] = event.target.value.trim();
+        formData.email = form.email.value.trim();
+        formData.message = form.message.value.trim();
+
+        if (formData.email === "" || formData.message === "") {
+            alert('Fill please all fields');
+            return;
+        }
+ localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+
+       
+        // const form = event.currentTarget;
+        localStorage.removeItem(STORAGE_KEY);
     
-    form.reset();
-};
+        form.reset();
+        formData = { email: '', message: '' };
+    };
 
-textarea.addEventListener('input', handleMessageInput);
-function handleMessageInput(event) {
-    console.log(event.target.value);
-}
+    textarea.addEventListener('input', handleMessageInput);
+
+    function handleMessageInput(event) {
+
+        console.log(event.target.value);
+
+          formData.message = event.target.value;
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+        
+
+        let data;
+        if (typeof event.target.value !== "string") {
+            data = JSON.stringify(event.target.value);
+        }
+        data = event.target.value;
+
+        localStorage.setItem(STORAGE_KEY, data)
+
+        
+    }
+
+});
